4ff5059c900a81738adfca3da6bba5af9188a012
magiccoder890 <23fzhu@athenian.org>
2020-09-09 14:38:35 +0000

    This is Bert's homework.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,28 +2,66 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        String str2 = str.toLowerCase();
+        //System.out.print(str2.length());
+        int a_number = 0;
+        int b_number = 0;
+        int c_number = 0;
+        for (int i = 0; i < str2.length();i++) {
+            char Detected_Character = str2.charAt(i);
+            if ("a".indexOf(Detected_Character) == 0) {
+                a_number = a_number + 1;
+            }
+            else if ("b".indexOf(Detected_Character) == 0) {
+                b_number = b_number + 1;
+            }
+            else if ("c".indexOf(Detected_Character) == 0) {
+                c_number = c_number + 1;
+
+            }
+        }
+        System.out.print("There are "+ a_number + " as, and " + b_number + " bs, and " + c_number + " cs.");
+        return str2.length();
     }
 
-    // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        if (str.contains("the")) {
+            System.out.print("Yes, it has the");
+        }
+        if (str.contains("The")){
+            System.out.print("Yes, it has The");
+        }
+        return true;
+
     }
 
-    // Checks whether str is a palindrome or not
-    public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+    public static boolean palindrome(String str) {
+        char[] chr = str.toCharArray();
+        int index_num = 0;
+        for (int i = chr.length - 1; i >= 0; i--) {
+            if (str.charAt(index_num) != chr[i]) {
+                System.out.print("No, it not a palindrome");
+                break;
+            }
+            else {
+                index_num = index_num + 1;
+            }
+        }
+        if (index_num == str.length()) {
+            System.out.print("Yes, it is a palindrome");
+        }
+        return true;
     }
-    
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
+    public static void main (String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input a string for us to detect");
+        String str = scanner.nextLine();
+        countABC(str);
+        System.out.print("\n");
+        containsThe(str);
+        System.out.print("\n");
+        palindrome(str);
     }
 }
