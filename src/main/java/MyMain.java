10f5dac081c194800ad16e6382291f6f9832b34a
magiccoder890 <23fzhu@athenian.org>
2020-09-10 22:35:41 +0000

    This is Bert's fixed homework.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -22,30 +22,35 @@ public class MyMain {
             }
         }
         System.out.print("There are "+ a_number + " as, and " + b_number + " bs, and " + c_number + " cs.");
-        return str2.length();
+        return a_number;
+        
     }
 
     public static boolean containsThe(String str) {
         if (str.contains("the")) {
             System.out.print("Yes, it has the");
+            return true;
         }
         if (str.contains("The")){
             System.out.print("Yes, it has The");
+            return true;
         }
-        return true;
-
+        else {
+            return false;
+        }
     }
 
-    public static boolean palindrome(String str) {
+    public static boolean isPalindrome(String str) {
         char[] chr = str.toCharArray();
         int index_num = 0;
         for (int i = chr.length - 1; i >= 0; i--) {
             if (str.charAt(index_num) != chr[i]) {
                 System.out.print("No, it not a palindrome");
-                break;
+                return false;
             }
             else {
                 index_num = index_num + 1;
+                return true;
             }
         }
         if (index_num == str.length()) {
@@ -62,6 +67,7 @@ public class MyMain {
         System.out.print("\n");
         containsThe(str);
         System.out.print("\n");
-        palindrome(str);
+        isPalindrome(str);
+        scanner.close();
     }
 }
